ca4e90ea206b85f79bb34fc95e5b3bb10cee8610
ksheng- <ksheng7746@gmail.com>
2017-02-09 13:42:19 -0500

    created files for MAC

---
+++ mac/mac.srcs/sources_1/new/mac.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/09/2017 01:41:36 PM
+-- Design Name: 
+-- Module Name: mac - behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity mac is
+--  Port ( );
+end mac;
+
+architecture behavioral of mac is
+
+begin
+
+
+end behavioral;
