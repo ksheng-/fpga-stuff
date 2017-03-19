522ebacabbc4200d34801b5a45621e9e1f3e9f07
ksheng- <ksheng7746@gmail.com>
2017-03-19 13:31:54 -0400

    fixed gitignore

--- fp/fp.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=D:\\Xilinx\\Vivado\\2016.4\\bin
-call %xv_path%/xelab  -wto b720a45f70fd4a45814052a1fdcb7d19 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot fpmultiplier_tb_behav xil_defaultlib.fpmultiplier_tb -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
